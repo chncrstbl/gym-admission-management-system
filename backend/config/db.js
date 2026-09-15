f02e4e8b55a36b4f5b5cf8fff2eda73cd7e67c77
Nen <cristobalchristinand2906@gmai.com>
2026-09-15 14:05:08 +0800

    "Backend database changes"

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,17 @@
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
-dotenv.config(); 
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,       
-    user: process.env.DB_USER,       
+dotenv.config();
+
+const db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false // Required for cloud database connections like Aiven
+    }
 });
 
-export default pool.promise();
+export default db;
