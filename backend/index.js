a06c9d91af5ca637e2b0120c338ca789042d0638
Nen <cristobalchristinand2906@gmai.com>
2026-09-15 13:05:13 +0800

    "Minor Changes"

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,6 @@ import equipmentRoutes from './routes/equipmentRoutes.js';
 
 dotenv.config();
 
-const express = require('express')
 const app = express();
 
 // Middleware
@@ -27,6 +26,6 @@ app.use('/api', authRoutes);
 
 const PORT = process.env.PORT || 5000; app.listen(PORT);
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
