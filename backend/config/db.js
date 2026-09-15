34c5eebcab98bcd3b397bfcb904eb31d5a69d601
Nen <cristobalchristinand2906@gmai.com>
2026-09-15 13:46:02 +0800

    "Database Changes"

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,6 @@
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
+
 dotenv.config(); 
 
 const pool = mysql.createPool({
@@ -7,6 +8,10 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,       
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    },
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
